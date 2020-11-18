ddaecfac699ae165eb5e4dad894b4072279ebfee
AbuAhmed1 <71983256+AbuAhmed1@users.noreply.github.com>
2020-11-18 14:26:50 +0000

    add products, remove and print

--- app05-stock/StockApp.java
+++ app05-stock/StockApp.java
@@ -4,35 +4,35 @@
  * stock manager so that users can add, edit,
  * print and remove stock products
  *
- * @author Student Name
+ * @author Abu Ahmed
  * @version 0.1
  */
 public class StockApp
 {
+    public final int FIRST_ID = 200;
     // Use to get user input
     private InputReader input;
     
+    private StockManager manager;
+    
+    private StockDemo demo;
+    
+    private int nextID = FIRST_ID;
+  
     /**
      * Constructor for objects of class StockApp
      */
     public StockApp()
     {
         input = new InputReader();
+        manager = new StockManager();
+        demo = new StockDemo(manager);
     }
 
     /**
      * 
      */
     public void run()
-    {
-        printHeading();
-        getMenuChoice();
-    }
-    
-    /**
-     * 
-     */
-    public void getMenuChoice()
     {
         boolean finished = false;
         
@@ -42,11 +42,64 @@ public class StockApp
             printMenuChoices();
            
             String choice = input.getInput();
-            finished = true;
+            choice = choice.toLowerCase();
+            
+            executeMenuChoice(choice);
+            
+            if(choice.equals("Quit"))
+                finished = true;
         }
     }
     
-   
+    
+    /**
+     * 
+     */
+     public void executeMenuChoice(String choice)
+    {
+        if(choice.equals("add"))
+        {
+           addProduct();
+        }
+        else if(choice.equals("remove"))
+        {
+           removeProduct();
+        }
+        else if(choice.equals("printall"))
+        {
+           printAllProducts();
+        }
+        
+    }
+    
+    public void addProduct()
+    {
+         System.out.println("Add a new Product");
+        System.out.println();
+        
+        System.out.println("Please enter the name of the product ");
+        String name = input.getInput();
+        
+        Product product = new Product(nextID, name);
+        manager.addProduct(product);
+        
+        System.out.println("/nAdded" + product + " to the stock\n");
+        nextID++;
+    }
+    
+    public void removeProduct()
+    {
+        System.out.println("Remove a new Product");
+        System.out.println();
+        
+        System.out.println("Please enter the id of product ");
+        String number = input.getInput();
+        
+        int id = Integer.parseInt(number);
+         manager.removeProduct(id);
+        
+    }
+    
     /**
      * Print out a menu of operation choices
      */
@@ -60,6 +113,11 @@ public class StockApp
         System.out.println();        
     }
     
+    public void printAllProducts()
+    {
+        manager.printAllProducts();
+    }
+    
     /**
      * Print the title of the program and the authors name
      */
@@ -67,7 +125,7 @@ public class StockApp
     {
         System.out.println("******************************");
         System.out.println(" Stock Management Application ");
-        System.out.println("    App05: by Student Name");
+        System.out.println("    App05: by Abu Ahmed");
         System.out.println("******************************");
     }
 }
