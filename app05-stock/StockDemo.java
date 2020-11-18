4f194ea763a9070c610be0b9122e201be58bbaeb
AbuAhmed1 <71983256+AbuAhmed1@users.noreply.github.com>
2020-11-18 14:27:01 +0000

    Create StockDemo.java

---
+++ app05-stock/StockDemo.java
@@ -0,0 +1,111 @@
+/**
+ * Demonstrate the StockManager and Product classes.
+ * The demonstration becomes properly functional as
+ * the StockManager class is completed.
+ * 
+ * @author Abu Ahmed
+ * @version 22011615
+ */
+public class StockDemo
+{
+    // The stock manager.
+    private StockManager manager;
+
+    /**
+     * Create a StockManager and populate it with a few
+     * sample products.
+     */
+    public StockDemo(StockManager manager)
+    {
+        this.manager = manager;
+        
+        manager.addProduct(new Product(101, "Volkswagen Golf MK5"));
+        manager.addProduct(new Product(102,  "Honda Civic Type R"));
+        manager.addProduct(new Product(103,  "Nissan Micra"));
+        manager.addProduct(new Product(104,  "Bently Continental"));
+        manager.addProduct(new Product(105,  "Hyundai I30"));
+        manager.addProduct(new Product(106,  "Ford Focus ST"));
+        manager.addProduct(new Product(107,  "Telsa Model S"));
+        manager.addProduct(new Product(108,  "Jaguar XF"));
+        manager.addProduct(new Product(109,  "Seat Ibifa"));
+        manager.addProduct(new Product(110,  "Vauxhall Corsa"));
+    }
+    
+        /**
+     * This method will run the test needed to show that products can
+     * be delivered. This method meets the requirements.
+     */
+    public void runDemo()
+    {
+       manager.printAllProducts();
+       demoDeliverProducts();
+       demoSellProducts();
+       demoRemoveProduct();
+    }
+    
+    /**
+     * Provide a very simple demonstration of how a StockManager
+     * might be used. Details of one product are shown, the
+     * product is restocked, and then the details are shown again.
+     */
+    public void demoDeliverProducts()
+    {
+        // Show details of all of the products.
+        manager.printAllProducts();
+        // Take delivery of 5 items of one of the products.
+        manager.delivery(101, 10);
+        manager.delivery(102,10);
+        manager.delivery(103,10);
+        manager.delivery(104,10);
+        manager.delivery(105,10);
+        manager.delivery(106,10);
+        manager.delivery(107,10);
+        manager.delivery(108,10);
+        manager.delivery(109,10);
+        manager.delivery(110,10);
+        manager.printAllProducts();
+    }
+
+    public void demoSellProducts()
+    {
+        System.out.println("\nDemo of Selling\n");
+        int amount = 0;
+        
+        for(int id = 101; id <= 110; id++)
+        {
+            amount++;
+            manager.sellProduct(id, amount);
+        }
+    }
+    
+    public void demoRemoveProduct()
+    {
+        manager.removeProduct(104);
+    }
+    
+    /**
+     * Get the product with the given id from the manager.
+     * An error message is printed if there is no match.
+     * @param id The ID of the product.
+     * @return The Product, or null if no matching one is found.
+     */
+    public Product getProduct(int id)
+    {
+        Product product = manager.findProduct(id);
+        
+        if(product == null) 
+        {
+            System.out.println("Product with ID: " + id +
+                               " is not recognised.");
+        }
+        return product;
+    }
+
+    /**
+     * @return The stock manager.
+     */
+    public StockManager getManager()
+    {
+        return manager;
+    }
+}
