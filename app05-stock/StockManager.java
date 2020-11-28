db9be52b65d6915c03cb7e7c9b308d325fab42c6
AbuAhmed1 <71983256+AbuAhmed1@users.noreply.github.com>
2020-11-28 15:20:53 +0000

    Update StockManager.java

--- app05-stock/StockManager.java
+++ app05-stock/StockManager.java
@@ -28,7 +28,7 @@ public class StockManager
     {
         stock.add(item);
     }
-    
+
     /**
      * Receive a delivery of a particular product.
      * Increase the quantity of the product by the given amount.
@@ -48,9 +48,8 @@ public class StockManager
             System.out.println("Product ID" +  id +  "NOT FOUND!!!");
         }
     }
-    
-    
-     /**
+
+    /**
      * Sell one of the given item.
      * Show the before and after status of the product.
      * @param id The ID of the product being sold.
@@ -58,7 +57,7 @@ public class StockManager
     public void sellProduct(int id, int amount)
     {
         Product product = findProduct(id);
-        
+
         if(product != null) 
         {
             printDetails(id);
@@ -66,8 +65,8 @@ public class StockManager
             printDetails(id);
         }
     }
-    
-     /**
+
+    /**
      * Show details of the given product. If found,
      * its name and stock quantity will be shown.
      * @param id The ID of the product to look for.
@@ -75,15 +74,15 @@ public class StockManager
     public void printDetails(int id)
     {
         Product product = findProduct(id);
-        
+
         if(product != null) 
         {
             System.out.println(product.toString());
         }
     }
-    
-        public void removeProduct(int id)
-     {
+
+    public void removeProduct(int id)
+    {
         Product product = findProduct(id);
         if(product != null) 
         {
@@ -91,8 +90,7 @@ public class StockManager
             stock.remove(product);
         }
     }
-    
-    
+
     /**
      * Try to find a product in the stock with the given id.
      * @return The identified product, or null if there is none
@@ -102,29 +100,29 @@ public class StockManager
     {
         for(Product product : stock)
         {
-        if(product.getID() == id)
-        {
-           return product;
+            if(product.getID() == id)
+            {
+                return product;
+            }
         }
+
+        return null;
     }
-     
-    return null;
-    }
-    
-        public boolean isDuplicateID(int id)
+
+    public boolean isDuplicateID(int id)
     {
         for(Product product : stock)
         {
-        if(product.getID() == id)
-        {
-           return true;
+            if(product.getID() == id)
+            {
+                return true;
+            }
         }
+
+        return false;
     }
-     
-    return false;
-    }
-    
-   /**
+
+    /**
      * Locate a product with the given ID, and return how
      * many of this item are in stock. If the ID does not
      * match any product, return zero.
@@ -141,21 +139,21 @@ public class StockManager
      */
     public void printAllProducts()
     {
-         System.out.println();
-         System.out.println("Abu's Stock List");
+        System.out.println();
+        System.out.println("Abu's Stock List");
         System.out.println("=================");
         System.out.println();
-        
+
         if(stock.size() ==0)
         {
             System.out.println("Currently you have no stock!"); 
         }
         else
         {
-        for(Product product :stock)
-        {
-            System.out.println(product);
-        }
+            for(Product product :stock)
+            {
+                System.out.println(product);
+            }
         }
     }
 }
